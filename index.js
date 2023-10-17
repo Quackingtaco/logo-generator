3c3d371672ed64a27c64491ea130b432ac72a5c3
Quackingtaco <r2kayla@gmail.com>
2023-10-16 21:44:27 -0400

    updated index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const inquirer = require("inquirer");
+const {triangle, circle, square} = require("./lib/shapes");
+const fs = require("fs");
+
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Text: Enter up to 3 characters:",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Text color: Enter a color keyword/hexadecimal number:",
+    },
+    {
+        type: "input",
+        name: "color",
+        message: "Shape color: Enter a color keyword/hexadecimal number:",
+    },
+    {
+        type: "list",
+        name: "image",
+        message: "Choose which image to use",
+        choices: ["triangle", "circle", "square"],
+    },
+];
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        generateLogo(answers);
+    });
+}
+
+function generateLogo(answers) { 
+    if (answers.image === "triangle") {
+        const shape = new triangle(answers.color, answers.text, answers.textColor);
+    } else if (answers.image === "circle") {
+        const shape = new circle(answers.color, answers.text, answers.textColor);
+    } else (answers.image === "square") {
+        const shape = new square(answers.color, answers.text, answers.textColor);
+    }
+    writeToFile(shape);
+}
+
+function writeToFile(shape) {
+    const svgCode = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}</svg>`;
+    fs.writeFile("./dist/shape.svg", svgCode, (err) => {
+        if (err) {
+            console.log(err);
+        }
+
+        else {
+            console.log("Congrats! You have made a logo!");
+        }
+        
+    });
+}
